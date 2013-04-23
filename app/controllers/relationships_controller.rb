4e675e13aa63c2ad0ff296957f388b7d39071f8a
Emilio Losada <elosada@lichencommunications.com>
2013-04-23 18:07:53 -0400

    last

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,9 +1,12 @@
 class RelationshipsController < ApplicationController
   before_filter :signed_in_user
 
+  #respond_to :html, :js
+
   def create
     @user = User.find(params[:relationship][:followed_id])
     current_user.follow!(@user)
+    #respond_with @user
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
@@ -13,6 +16,7 @@ class RelationshipsController < ApplicationController
   def destroy
     @user = Relationship.find(params[:id]).followed
     current_user.unfollow!(@user)
+    #respond_with @user
     respond_to do |format|
       format.html { redirect_to @user }
       format.js
